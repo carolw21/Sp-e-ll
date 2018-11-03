0ba1cf1f6269ae57a3c12aef1390cc0658746636
carolw21 <carolw2000@gmail.com>
2018-11-03 16:22:44 -0700

    s

--- js/main.js
+++ js/main.js
@@ -4,13 +4,13 @@ beginBtn.onclick = startPractice;
 var wordarr = [];
 
 function startPractice() {
+  wordarr = [];
   wordarr.push(document.getElementById('input1').value);
   wordarr.push(document.getElementById('input2').value);
   wordarr.push(document.getElementById('input3').value);
   wordarr.push(document.getElementById('input4').value);
   wordarr.push(document.getElementById('input5').value);
   wordarr.push(document.getElementById('input6').value);
-  console.log(wordarr);
   document.getElementById('page2').style.display = "block";
   document.getElementById('page1').style.display = "none";
 }
