d5a6c299839b390a7532cd6b3b9ea97d087d87d8
carolw21 <carolw2000@gmail.com>
2018-11-03 16:21:39 -0700

    Sdf

---
+++ js/main.js
@@ -0,0 +1,16 @@
+var beginBtn = document.getElementById('begin');
+beginBtn.onclick = startPractice;
+
+var wordarr = [];
+
+function startPractice() {
+  wordarr.push(document.getElementById('input1').value);
+  wordarr.push(document.getElementById('input2').value);
+  wordarr.push(document.getElementById('input3').value);
+  wordarr.push(document.getElementById('input4').value);
+  wordarr.push(document.getElementById('input5').value);
+  wordarr.push(document.getElementById('input6').value);
+  console.log(wordarr);
+  document.getElementById('page2').style.display = "block";
+  document.getElementById('page1').style.display = "none";
+}
