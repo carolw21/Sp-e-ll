f54394bae433b828cfb1e6336b409715cc83b698
carolw21 <carolw2000@gmail.com>
2018-11-03 21:22:01 -0700

    SDf

--- js/firebase.js
+++ js/firebase.js
@@ -1,24 +1,26 @@
 var provider = new firebase.auth.GoogleAuthProvider();
 
-firebase.auth().signInWithPopup(provider).then(function(result) {
-// This gives you a Google Access Token. You can use it to access the Google API.
-var token = result.credential.accessToken;
-// The signed-in user info.
-var user = result.user;
-
 var missedWords = [];
+var token;
+var user;
 
-}).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
+firebase.auth().signInWithPopup(provider).then(function(result) {
+  // This gives you a Google Access Token. You can use it to access the Google API.
+  token = result.credential.accessToken;
+  // The signed-in user info.
+  user = result.user;
 
-    console.log(errorCode);
-    console.log(errorMessage);
-  // The email of the user's account used.
-  var email = error.email;
-  // The firebase.auth.AuthCredential type that was used.
-  var credential = error.credential;
+  }).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+
+      console.log(errorCode);
+      console.log(errorMessage);
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
 });
 
 function writeUserData(missedWords) {
@@ -37,8 +39,6 @@ function readUserData() {
 }
 
 function getMissedWords() {
-  var missedWords = readUserData();
-  console.log(missedWords);
   return missedWords;
 }
 
