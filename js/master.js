0b493595dbb034a1714ce31fed2f0d6c5dad5bac
carolw21 <carolw2000@gmail.com>
2018-11-03 20:56:04 -0700

    sdf

--- js/master.js
+++ js/master.js
@@ -15,6 +15,23 @@ function LoadScriptsSync (_scripts, scripts) {
     loopArray(_scripts, scripts);
 }
 
+// load script function with callback to handle synchronicity
+function loadScript( src, script, callback ){
+
+    script = document.createElement('script');
+    script.onerror = function() {
+        // handling error when loading script
+        alert('Error to handle')
+    }
+    script.onload = function(){
+        console.log(src + ' loaded ')
+        callback();
+    }
+    script.src = src;
+    document.getElementsByTagName('head')[0].appendChild(script);
+}
+
+
 var scripts = [];
 var _scripts = ['firebase.js', 'main.js'];
 LoadScriptsSync(_scripts, scripts);
